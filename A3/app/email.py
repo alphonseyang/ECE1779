e60543ee1a7ad1582303b712064b8584fcff5f91
Chengchen Yang <chengchen.yang@veeva.com>
2020-11-28 16:29:48 -0500

    change to use sns for email service

--- A3/app/email.py
+++ A3/app/email.py
@@ -34,15 +34,20 @@ def work():
 # helper method to subscribe the service
 def process_subscribe(email_address, country, frequency):
     try:
+        topic_arn = get_sns_topic(country, frequency)
+        if not topic_arn:
+            raise Exception("Failed to create/locate topic")
+        sub_arn = register_email(email_address, topic_arn)
         ddb.put_item(
             TableName=constants.SUBSCRIPTION_DDB_TABLE,
             Item={
                 "email": {"S": email_address},
                 "country": {"S": country},
-                "frequency": {"S": frequency}
+                "frequency": {"S": frequency},
+                "sub_arn": {"S": sub_arn}
             }
         )
-        flash("Successfully Subscribed", constants.INFO)
+        flash("Subscription Email Sent, Please Confirm", constants.INFO)
     except Exception as e:
         print(e)
         flash("Failed to Subscribe, Please Try Again", constants.ERROR)
@@ -62,6 +67,10 @@ def process_unsubscribe(email_address, country):
         if not response.get("Item"):
             flash("Please Subscribe First", constants.ERROR)
             return
+        error = deregister_email(response["Item"]["sub_arn"]["S"])
+        if error:
+            flash(error, constants.ERROR)
+            return
         ddb.delete_item(
             TableName=constants.SUBSCRIPTION_DDB_TABLE,
             Key=key
@@ -75,4 +84,40 @@ def process_unsubscribe(email_address, country):
 # get the countries list that can be used
 def get_supported_countries():
     # TODO: this should be dynamic
-    return ["Canada", "China", "United States", "Japan", "South Korea", "United Kingdom", "France", "Germany", "Mexico", "India"]
+    return ["Canada", "China", "United States", "Japan", "South Korea",
+            "United Kingdom", "France", "Germany", "Mexico", "India"]
+
+
+# create topic if not exist
+def get_sns_topic(country, freq):
+    sns = boto3.client("sns")
+    response = sns.list_topics()
+    topics = response.get("Topics", list())
+    new_topic = country.replace(" ", "_") + "-" + freq
+    for topic in topics:
+        if new_topic in topics:
+            return topic
+    response = sns.create_topic(Name=new_topic)
+    return response.get("TopicArn")
+
+
+# register an email to subscribe
+def register_email(email, topic_arn):
+    sns = boto3.client("sns")
+    response = sns.subscribe(
+        TopicArn=topic_arn,
+        Protocol="email",
+        Endpoint=email,
+        ReturnSubscriptionArn=True
+    )
+    return response["SubscriptionArn"]
+
+
+# deregister an email from SNS topic
+def deregister_email(sub_arn):
+    sns = boto3.client("sns")
+    response = sns.get_subscription_attributes(SubscriptionArn=sub_arn)
+    if response["Attributes"]["PendingConfirmation"] == "false":
+        sns.unsubscribe(SubscriptionArn=sub_arn)
+    else:
+        return "Please confirm subscription before unsubscribe"
--- A3/background_workers/subscription_starter.py
+++ A3/background_workers/subscription_starter.py
@@ -8,7 +8,6 @@ SQS_QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/752103853538/subscription"
 EMAIL_WORKER_BATCH_SIZE = 10000
 SUBSCRIPTION_DDB_TABLE = "subscription"
 
-
 '''
 scan the DDB subscription table to check for the emails that need to have the email sent 
 (will need to create secondary index for mass customer situation in the future)
@@ -18,9 +17,48 @@ create sqs message and let worker do the work.
 
 def lambda_handler(event, context):
     freq = event.get("frequency", "daily")
-    emails_countries = get_emails_countries(freq)
-    for country in emails_countries:
-        create_worker_tasks(country, emails_countries[country], freq)
+    # emails_countries = get_emails_countries(freq)
+    # for country in emails_countries:
+    #     create_worker_tasks(country, emails_countries[country], freq)
+    topic_arns = get_topic_arns(freq)
+    for arn in topic_arns:
+        country = arn.split(":")[-1].split("-")[0].replace("_", " ")
+        publish_message(arn, country, freq)
+
+
+# get the topic arns that will need to publish new messages to the subscribed users
+def get_topic_arns(freq):
+    countries = get_supported_countries()
+    topic_names = set([country.replace(" ", "_") + "-" + freq for country in countries])
+    sns = boto3.client("sns")
+    response = sns.list_topics()
+    topic_arns = list()
+    for topic in response.get("Topics", list()):
+        should_remove = None
+        for name in topic_names:
+            if name in topic["TopicArn"]:
+                topic_arns.append(topic["TopicArn"])
+                should_remove = name
+                break
+        if should_remove:
+            topic_names.remove(should_remove)
+    return topic_arns
+
+
+# publish message to the user
+def publish_message(topic_arn, country, freq):
+    pass
+
+
+def get_supported_countries():
+    return ["Canada", "China", "United States", "Japan", "South Korea",
+            "United Kingdom", "France", "Germany", "Mexico", "India"]
+
+
+'''
+Original design using SES service, which is not available for the educate account
+keep the code just for reference
+'''
 
 
 # extract all the emails that need to have the email send
@@ -41,7 +79,7 @@ def create_worker_tasks(country, emails, freq):
     sqs = boto3.client("ses")
     bulk = EMAIL_WORKER_BATCH_SIZE
     for i in range(len(emails), bulk):
-        bulk_emails = emails[i:i+bulk]
+        bulk_emails = emails[i:i + bulk]
         sqs.send_message(
             QueueUrl=SQS_QUEUE_URL,
             MessageBody=json.dumps({
